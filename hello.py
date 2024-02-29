b77bc5ec504c05bf5fab5b746ee26a5bbf04f573
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-02-29 21:46:08 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("hello world")
 print("how the freeaking hell does github work actually kill me end my life right now")
 print('this is my third commit lemme try the second branching option thingy thing')
+print("hello world on second branch")
